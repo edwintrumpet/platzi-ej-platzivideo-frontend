e94fc6d7375dd8e38b721ed10243badccb65910b
Edwin García <spark.com.co@gmail.com>
2019-11-22 06:35:25 -0500

    intersection observer

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,13 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 const video = document.querySelector('video')
 const playButton = document.querySelector('#play-button')
 const muteButton = document.querySelector('#mute-button')
 
 const player = new MediaPlayer({ el: video, plugins: [
-    new AutoPlay()
+    new AutoPlay(),
+    new AutoPause()
 ] })
 
 playButton.onclick = () => player.tooglePlay()
