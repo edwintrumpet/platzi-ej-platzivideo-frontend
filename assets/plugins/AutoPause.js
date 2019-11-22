952c4ce923d9037982dd139f06cc3fb3faee92e0
Edwin García <spark.com.co@gmail.com>
2019-11-22 06:51:20 -0500

    visibilitychange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor() {
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this)
+        this.handlerVisibilityChange = this.handlerVisibilityChange.bind(this)
     }
 
     run(player) {
@@ -11,12 +12,23 @@ class AutoPause {
         })
 
         observer.observe(this.player.media)
+
+        document.addEventListener('visibilitychange', this.handlerVisibilityChange)
     }
 
     handleIntersection(entries) {
         const entry = entries[0]
         const isVisible = entry.intersectionRatio >= this.threshold
 
+        if(isVisible) {
+            this.player.play()
+        }else {
+            this.player.pause()
+        }
+    }
+
+    handlerVisibilityChange() {
+        const isVisible = document.visibilityState === 'visible'
         if(isVisible) {
             this.player.play()
         }else {
