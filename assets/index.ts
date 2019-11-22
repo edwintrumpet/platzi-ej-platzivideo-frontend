bb03d06dbd2b6b2de2d9ddb87aadd039646fa76f
Edwin García <spark.com.co@gmail.com>
2019-11-22 16:48:16 -0500

    ads plugin

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,15 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import AdsPlugin from './plugins/Ads'
 const video = document.querySelector('video')
 const playButton: HTMLElement = document.querySelector('#play-button')
 const muteButton: HTMLElement = document.querySelector('#mute-button')
 
 const player = new MediaPlayer({ el: video, plugins: [
     new AutoPlay(),
-    new AutoPause()
+    new AutoPause(),
+    new AdsPlugin()
 ] })
 
 playButton.onclick = () => player.tooglePlay()
