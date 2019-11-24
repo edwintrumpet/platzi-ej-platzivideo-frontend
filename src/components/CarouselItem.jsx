fe348b67cce5ebc550504ba18e8270251cbd8ea0
Edwin García <spark.com.co@gmail.com>
2019-11-23 21:05:01 -0500

    Iteración de items del carousel por medio de los datos obtenidos

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -4,16 +4,18 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className='carousel-item'>
-    <img className='carousel-item__img' src='https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='people' />
+    <img className='carousel-item__img' src={cover} alt={title} />
     <div className='carousel-item__details'>
       <div>
         <img src={playIcon} alt='play' />
         <img src={plusIcon} alt='add' />
       </div>
-      <p className='carousel-item__details--title'>Título descriptivo</p>
-      <p className='carousel-item__details--subtitle'>2019 16+ 114 minutos</p>
+      <p className='carousel-item__details--title'>{title}</p>
+      <p className='carousel-item__details--subtitle'>
+        {`${year} ${contentRating} ${duration} minutos`}
+      </p>
     </div>
   </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -10,46 +10,44 @@ import '../assets/styles/App.scss';
 const API = 'http://localhost:3000/initalState';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
-
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
   useEffect(() => {
     fetch(API)
       .then((response) => response.json())
       .then((data) => setVideos(data))
       .catch((err) => console.log(err));
   }, []);
-
-  console.log(videos);
-
   return (
     <div className='App'>
       <Header />
       <Search />
-
-      <Categories title='Mi lista'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {
+        videos.mylist.length > 0 && (
+          <Categories title='Mi lista'>
+            <Carousel>
+              <CarouselItem />
+            </Carousel>
+          </Categories>
+        )
+      }
 
       <Categories title='Tendencias'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {
+            videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
         </Carousel>
       </Categories>
 
       <Categories title='Originales de PlatziVideo'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {
+            videos.originals.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
         </Carousel>
       </Categories>
       <Footer />
