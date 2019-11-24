07cc8e8316e62f3a0948558c23221ca0343637d5
Edwin García <spark.com.co@gmail.com>
2019-11-23 19:12:24 -0500

    Agregando imágenes con webpack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
 
+import logo from '../assets/static/logo-platzi-video.png';
+import userIcon from '../assets/static/user-icon.png';
+
 const Header = () => (
   <header className='header'>
-    <img className='header__img' src='../assets/logo-platzi-video.png' alt='logo platzi video' />
+    <img className='header__img' src={logo} alt='logo platzi video' />
     <div className='header__menu'>
       <div className='header__menu--profile'>
-        <img src='../assets/user-icon.png' alt='user' />
+        <img src={userIcon} alt='user' />
         <p>Perfil</p>
       </div>
       <ul>
