1e39a7bf4cf23aa4c4262c608bb8c8741f89f71c
Edwin García <spark.com.co@gmail.com>
2019-11-25 07:59:43 -0500

    Pasándole el id al player y modificación necesaria en webpack

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
 import '../assets/styles/containers/Player.scss';
+import NotFound from './NotFound';
 
-const Player = () => {
-  return (
+const Player = ({ history, match, playing, getVideoSource }) => {
+  const { id } = match.params;
+  const hasPlaying = Object.keys(playing).length > 0;
+
+  useEffect(() => {
+    getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
     <div className='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
-        <button type='button'>Regresar</button>
+        <button type='button' onClick={() => history.goBack()}>Regresar</button>
       </div>
     </div>
-  );
+  ) : <NotFound />;
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
 
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
