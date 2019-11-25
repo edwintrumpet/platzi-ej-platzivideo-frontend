5fda731bc783e58f69747e03fdfee923d3a67662
Edwin García <spark.com.co@gmail.com>
2019-11-24 23:32:18 -0500

    Componente de registro

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/containers/Register.scss';
 
-const Register = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Regístrate</h2>
-      <form className='register__container--form'>
-        <input aria-label='Nombre' type='text' className='input' placeholder='Nombre' />
-        <input aria-label='Correo' type='text' className='input' placeholder='Correo' />
-        <input aria-label='Contraseña' type='password' className='input' placeholder='Contraseña' />
-        <button type='submit' className='button'>Registrarme</button>
-      </form>
-      <Link className='register__container--iniciar-sesion' to='/login'>Iniciar sesión</Link>
+const Register = () => {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+  const handleInput = (event) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Regístrate</h2>
+        <form className='register__container--form' onSubmit={handleSubmit}>
+          <input
+            name='name'
+            aria-label='Nombre'
+            type='text'
+            className='input'
+            placeholder='Nombre'
+            onChange={handleInput}
+          />
+          <input
+            name='email'
+            aria-label='Correo'
+            type='text'
+            className='input'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            aria-label='Contraseña'
+            type='password'
+            className='input'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>Registrarme</button>
+        </form>
+        <Link className='register__container--iniciar-sesion' to='/login'>Iniciar sesión</Link>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Register;
