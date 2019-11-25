1c3ee8d4d208ff859f30930bf132e2a289b85526
Edwin García <spark.com.co@gmail.com>
2019-11-24 21:48:58 -0500

    Estado en el login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,43 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/containers/Login.scss';
 
-const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia sesión</h2>
-      <form className='login__container--form'>
-        <input aria-label='Correo' type='text' className='input' placeholder='Correo' />
-        <input aria-label='Contraseña' type='password' className='input' placeholder='Contraseña' />
-        <button type='submit' className='button'>Iniciar sesión</button>
-        <div className='login__container--remember-me'>
-          <label htmlFor='cbox1'>
-            <input type='checkbox' name='cbox1' id='cbox1' value='checkbox' />
+const Login = () => {
+  const [formValues, setFormValues] = useState({
+    email: '',
+  });
+
+  const handleInput = (event) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='email'
+            aria-label='Correo'
+            type='text'
+            className='input'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            aria-label='Contraseña'
+            type='password'
+            className='input'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>Iniciar sesión</button>
+          <div className='login__container--remember-me'>
+            <label htmlFor='cbox1'>
+              <input
+                type='checkbox'
+                name='cbox1'
+                id='cbox1'
+                value='checkbox'
+              />
             Recuérdame
-          </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} alt='Google' />
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} alt='Google' />
                     Inicia sesión con Google
-        </div>
-        <div>
-          <img src={twitterIcon} alt='Twitter' />
+          </div>
+          <div>
+            <img src={twitterIcon} alt='Twitter' />
                     Inicia sesión con Twitter
-        </div>
-      </section>
-      <p className='login__container--register'>
+          </div>
+        </section>
+        <p className='login__container--register'>
                 No tienes ninguna cuenta
-        {' '}
-        <Link to='/register'>Regístrate</Link>
-      </p>
+          {' '}
+          <Link to='/register'>Regístrate</Link>
+        </p>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Login;
 
