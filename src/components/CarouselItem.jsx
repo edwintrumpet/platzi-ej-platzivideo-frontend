59bfdb454d57821bffbd6d75d10e13a9957ee5a5
Edwin García <spark.com.co@gmail.com>
2019-11-24 19:32:18 -0500

    Agregar elementos al mi lista de favoritos

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { setFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
-  <div className='carousel-item'>
-    <img className='carousel-item__img' src={cover} alt={title} />
-    <div className='carousel-item__details'>
-      <div>
-        <img src={playIcon} alt='play' />
-        <img src={plusIcon} alt='add' />
+const CarouselItem = ({
+  id,
+  cover,
+  title,
+  year,
+  contentRating,
+  duration,
+  setFavorite,
+}) => {
+  const handleSetFavorite = () => {
+    setFavorite({
+      id, cover, title, year, contentRating, duration,
+    });
+  };
+
+  return (
+    <div className='carousel-item'>
+      <img className='carousel-item__img' src={cover} alt={title} />
+      <div className='carousel-item__details'>
+        <div>
+          <img src={playIcon} alt='play' />
+          <img
+            src={plusIcon}
+            alt='add'
+            onClick={handleSetFavorite}
+          />
+        </div>
+        <p className='carousel-item__details--title'>{title}</p>
+        <p className='carousel-item__details--subtitle'>
+          {`${year} ${contentRating} ${duration} minutos`}
+        </p>
       </div>
-      <p className='carousel-item__details--title'>{title}</p>
-      <p className='carousel-item__details--subtitle'>
-        {`${year} ${contentRating} ${duration} minutos`}
-      </p>
     </div>
-  </div>
-);
+  );
+};
 
 CarouselItem.propTypes = {
   cover: PropTypes.string,
@@ -29,4 +51,8 @@ CarouselItem.propTypes = {
   duration: PropTypes.number,
 };
 
-export default CarouselItem;
+const mapDispatchToProps = {
+  setFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarouselItem);
